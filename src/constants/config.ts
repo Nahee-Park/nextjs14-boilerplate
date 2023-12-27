2c6202cfbc58c17b52cc1abbf4a4f7e1f1e67e66
nahee-park <jessica396@ewhain.net>
2023-12-27 15:33:20 +0900

    initial commit

---
+++ src/constants/config.ts
@@ -0,0 +1,16 @@
+// Config file for your website
+export const SiteConfig = {
+  url: 'https://nahee-park.github.io',
+  pathPrefix: '/',
+  title: 'YOUR_SITE_TITLE',
+  subtitle: 'YOUR_SITE_SUBTITLE',
+  copyright: 'Nahee-Park © All rights reserved.',
+  googleAnalyticsId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+  author: {
+    name: 'Nahee-park',
+    photo: '/profile.png',
+    contacts: {
+      email: 'nahee.developer@gmail.com',
+    },
+  },
+};
